cb4d8c08d276d248e842f0a107e96f78e44c0bdf
polomono <polomono@naver.com>
2024-08-19 10:35:48 +0900

    Update .gitignore to exclude all files except itself

--- hello/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def sayHello(request, name):
-  html = "<h1>Hello, {}!</h1>".format(name)
-  return HttpResponse(html)
-
-# Create your views here.
